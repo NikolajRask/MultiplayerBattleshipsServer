ce52eb0a074f5dd2aab31d235ccbe0e2a0e7181a
Nikolaj Rask <83177244+NikolajRask@users.noreply.github.com>
2023-11-22 20:37:38 +0100

    Added attack

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
   
+fs.writeFile("./games.json", JSON.stringify({games: {}}), (err) => {if (err) throw err})
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
@@ -28,12 +29,12 @@ app.get('/src/app.js', (req, res) => {
       console.log('user disconnected');
     });
     
-    socket.on('requestGame', () => {    
+    socket.on('requestGame', (uuid) => {    
         fs.readFile('./games.json','utf-8', function (err, data) {
             const parsedData = JSON.parse(data)
             const id =  Math.floor(Math.random() * 10000000)
             const d = new Date()
-            parsedData.games["game"+ id] = {id: id, board1: '', board2: '', player1: undefined, player2: undefined, ongoing: false, time_played: d, ships1: '', ships2: '', moves1: [], moves2: [], winner: ""}
+            parsedData.games["game"+ id] = {id: id, board1: '', board2: '', player1: uuid, player2: undefined, ongoing: false, time_played: d, ships1: '', ships2: '', moves1: [], moves2: [], winner: "", createdBy: uuid, turn: true}
             fs.writeFile('./games.json', JSON.stringify(parsedData), (err) => {
                 if (err) throw err;
             })
@@ -45,13 +46,37 @@ app.get('/src/app.js', (req, res) => {
     socket.on('joinGame', (msg) => {
         fs.readFile('./games.json','utf-8', function (err, data) {
             const parsedData = JSON.parse(data)
+            if (parsedData.games["game"+msg[0]] != undefined) {
+              if (msg[1] != parsedData.games["game"+msg[0]].createdBy) {
+                parsedData.games["game"+msg[0]].player2 = msg[1]
+                fs.writeFile("./games.json", JSON.stringify(parsedData), (err) => {if (err) console.log(err);})
+                socket.emit('gameJoinedID', parsedData.games["game"+msg[0]]);
 
-            if (parsedData.games["game"+msg] != undefined) {
-                socket.emit('gameJoinedID', JSON.stringify(parsedData.games["game"+msg]));
+                //change this for a direct message to the user who created the game
+                socket.broadcast.emit('yourGameJoined', parsedData.games["game"+msg[0]])
+              } else {
+                socket.emit('gameJoinedID', '400');
+              }
             } else {
                 socket.emit('gameJoinedID', "404");
             }
         })
+    })
+
+    socket.on('makeMove', (info) => {
+      console.log(info)
+      if (info[1] != null) {
+        fs.readFile('./games.json','utf-8', function (err, data) {
+          if (info[1].player1 == info[2]) {
+            //change this for a direct message to the user who created the game
+            socket.broadcast.emit('opponentMove2', [info[1],info[0]])
+          }
+          if (info[1].player2 == info[2]) {
+            //change this for a direct message to the user who created the game
+            socket.broadcast.emit('opponentMove1', [info[1],info[0]])
+          }
+        })
+      }
     })
   });
 
